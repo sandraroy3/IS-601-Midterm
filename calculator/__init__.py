bba1e9bbce013202c59ac27737aa00e109a23c92
Sandra Roy <sandraroy1204@gmail.com>
2025-03-13 16:02:42 -0400

    Modified calculator with operations and tests as classes, instances

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,28 @@
-def add(a,b):
-    return a+b
+# Class for modules and namespace 
 
-def subtract(a,b):
-    return a-b
+from calculator.calculation import Calculation
+from calculator.operations import add, subtract, multiply, divide
+
+class Calculator:
+    @staticmethod # functions, and fn in class is method, 
+    # static method - globally accessible - but not used in classes/instances
+    # perform actions, 
+    def add(a,b):
+        calculation = Calculation(a, b, add) # instance and pass the add fn from the
+        return calculation.get_result()
+
+    @staticmethod
+    def subtract(a,b):
+        calculation = Calculation(a, b, subtract)
+        return calculation.get_result()
+
+    @staticmethod
+    def multiply(a,b):
+        calculation = Calculation(a, b, multiply)
+    
+        return calculation.get_result()
+    
+    @staticmethod
+    def divide(a,b):
+        calculation = Calculation(a, b, divide)
+        return calculation.get_result()
